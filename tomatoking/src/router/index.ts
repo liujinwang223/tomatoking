50389282ef328e2fb6a462381ea21c9032e58d78
刘金旺 <1856532485@163.com>
2023-11-09 20:55:19 +0800

    页面内容初步调整

--- tomatoking/src/router/index.ts
+++ tomatoking/src/router/index.ts
@@ -13,8 +13,32 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: () => import('../views/plan/index.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/login/index.vue')
     }
   ]
 })
+router.beforeEach((to,from,next)=>{
+  // 模拟url登陆地址导航栏截取
+  // const url='http://122.9.41.215/fk_move/MapprList?userid=1815&school=szht6666'
+  // let urlOne = url.split('?');
+  // let userInfo=urlOne[1].split('&');
+  // let userId =userInfo[0].replaceAll('userid=','');
+  // let qyId=userInfo[1].replaceAll('school=','');
+  const userInfoState: any = userInfoData();
+  const store = userInfoData()
+  if (to.path === '/login') {
+    next();
+  } else {
+    if (Object.keys(userInfoState.userInfo).length === 0) {
+      next('/login');
+    } else {
+      next();
+    }
+  }
 
+})
 export default router
