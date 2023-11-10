ffb2cf9846a185b77257714e02f0773b2795e2f9
刘金旺 <1856532485@163.com>
2023-11-10 10:52:20 +0800

    倒入组件库以及组件

--- tomatoking/src/main.ts
+++ tomatoking/src/main.ts
@@ -6,11 +6,14 @@ import App from './App.vue'
 import ArcoVue from '@arco-design/web-vue';
 import '@arco-design/web-vue/dist/arco.css';
 import router from './router'
-
+import ArcoVueIcon from '@arco-design/web-vue/es/icon';
+const IconFont = Icon.addFromIconFontCn({ src: 'https://at.alicdn.com/t/font_180975_ue66sq60vyd.js' });
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
-app.use(ArcoVue)
+app.use(createPinia());
+app.use(router);
+app.use(ArcoVue);
+app.use(ArcoVueIcon);
+app.use(IconFont);
 
 app.mount('#app')
